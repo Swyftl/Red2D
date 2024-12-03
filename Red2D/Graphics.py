795b2ce551492509f921d4369c189923da9ca1db
Swyftl <juniorjonswift@gmail.com>
2024-12-03 02:13:31 +0000

    Changed rendering to make the position be the center of the object, not the top right corner.

--- Red2D/Graphics.py
+++ Red2D/Graphics.py
@@ -5,5 +5,6 @@ class Graphics:
     def __init__(self, screen):
         self.Screen = screen
 
-    def render_rect(self, color, rect):
-        pygame.draw.rect(self.Screen, color, rect)
+    def render_rect(self, color, x, y, x_size, y_size):
+        new_rect = pygame.Rect(x-x_size/2, y-y_size/2, x_size, y_size)
+        pygame.draw.rect(self.Screen, color, new_rect)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import Red2D.Player
 
 Engine = Red2D.Engine(1280, 720)
 
-Character = Red2D.Player.Player(Engine.Graphics, 10, 10, 10, 10)
+Character = Red2D.Player.Player(Engine.Graphics, 0, 0, 10, 10)
 
 move_left = Red2D.Input.Input("a")
 move_right = Red2D.Input.Input("d")
@@ -13,21 +13,21 @@ move_down = Red2D.Input.Input("s")
 
 while Engine.running:
     # Character Movement
-    if move_up.is_pressed():
+    if move_up.is_key_down():
         Character.position.y -= 1
-    elif move_down.is_pressed():
+    elif move_down.is_key_down():
         Character.position.y += 1
-    if move_left.is_pressed():
+    if move_left.is_key_down():
         Character.position.x -= 1
-    elif move_right.is_pressed():
+    elif move_right.is_key_down():
         Character.position.x += 1
 
     # Set the frame up with background
     Engine.tick("White")
-    Engine.set_title("Frames Rendered: " + str(Engine.rendered_frames))
+    Engine.set_title("x: "+str(Character.position.x) + " Y: "+str(Character.position.y))
     # Render the player
     Character.render_player()
-    if move_up.just_pressed():
+    if move_up.is_just_pressed():
         print("Just Pressed")
     # Finish up rendering and send the frame to the display
     Engine.render_frame()
