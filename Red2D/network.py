1c1b159fe5d20bc5cf35e819cbdf4be57265f509
Swyftl <68863847+Swyftl@users.noreply.github.com>
2025-03-08 13:59:08 +0000

    updated network

--- Red2D/network.py
+++ Red2D/network.py
@@ -1,18 +1,3 @@
-import socket
-import Red2D.Logging as Logging
-
-class Network():
-    def __init__(self, port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.port = port
-
-        self.socket.bind(("", self.port))
-        print("socket binded to port", self.port)
-
-        self.socket.listen(5)
-        print("socket is listening")
-
-
 '''
 This is a package that will be implemented and used later on. This will aim to provide the user with the ability to create and use servers for
 their game. The main way this will be done is with the python socket package.
