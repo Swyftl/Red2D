b3f2f0ef2c4124b5b7147da148b0e0d24e8f31a7
Swyftl <68863847+Swyftl@users.noreply.github.com>
2025-03-08 14:21:24 +0000

    fixed configs

--- Red2D/config.py
+++ Red2D/config.py
@@ -1,19 +1,22 @@
 # used for storing data in a configuration file for use across the game. This will be stored in a .env file
 
 import os
-import dotenv
+from dotenv import load_dotenv, set_key
 
 class config:
     def __init__(self):
+
+        load_dotenv()
+
         # Check for the conf file, create a new one if it doesn't exist
         if not os.path.exists('./_internal/Assets/config'):
             os.mkdir('./_internal/Assets/config')
             with open('./_internal/Assets/config/config.env', 'w') as f:
                 f.write("")
                 f.close()
-                self.config = ',/internal/Assets/config/config.env'
+                self.config = './_internal/Assets/config/config.env'
         else:
-            self.config = ',/internal/Assets/config/config.env'
+            self.config = './_internal/Assets/config/config.env'
 
 
     def get(self, key):
@@ -28,6 +31,6 @@ class config:
         # Set the given key to the given value
         # It will create the key if it doesn't exist
         if os.getenv(key):
-            dotenv.set_key(self.config, key, value)
+            set_key(self.config, key, value)
         else:
-            dotenv.set_key(self.config, key, value)
+            set_key(self.config, key, value)
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import Red2D
 import Red2D.Input
 import Red2D.Logging
+import Red2D.config
+
+config = Red2D.config.config()
+
+config.set("window_x", "1280")
+config.set("window_y", "720")
+
+config.set("framerate", "60")
+config.set("background_color", "white")
 
 Engine = Red2D.Engine(1280, 720)
 
